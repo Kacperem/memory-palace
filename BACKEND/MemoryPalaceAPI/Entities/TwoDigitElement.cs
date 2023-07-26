94b2a1e039e8947f26b692fa5826868086c94abf
mike <mikolajpaziewskialfa@gmail.com>
2023-07-26 12:45:09 +0200

    Add basic db structure

---
+++ BACKEND/MemoryPalaceAPI/Entities/TwoDigitElement.cs
@@ -0,0 +1,12 @@
+﻿namespace MemoryPalaceAPI.Entities
+{
+    public class TwoDigitElement
+    {
+        public int Id { get; set; }
+        public string Number { get; set; }
+        public string Text { get; set; }
+
+        public int TwoDigitSystemId { get; set; }
+        public virtual TwoDigitSystem TwoDigitSystem { get; set; }
+    }
+}
