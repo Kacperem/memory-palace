2ef3f514c3212c3419589b324b431528d963aee9
mike-papa <73196026+mike-papa@users.noreply.github.com>
2023-07-29 15:26:15 +0200

    Add TwoDigitSystem CRUD

---
+++ BACKEND/MemoryPalaceAPI/Models/TwoDigitElementDto.cs
@@ -0,0 +1,8 @@
+﻿namespace MemoryPalaceAPI.Models
+{
+    public class TwoDigitElementDto
+    {
+        public string Number { get; set; }
+        public string Text { get; set; }
+    }
+}
---
+++ BACKEND/MemoryPalaceAPI/Models/TwoDigitSystemDto.cs
@@ -0,0 +1,10 @@
+﻿using MemoryPalaceAPI.Entities;
+
+namespace MemoryPalaceAPI.Models
+{
+    public class TwoDigitSystemDto
+    {
+        public int Id { get; set; }
+        public List<TwoDigitElementDto> TwoDigitElements { get; set; }
+    }
+}
