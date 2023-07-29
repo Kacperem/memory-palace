f9fe25ea87b58a46e72f097ccafcb222fe3f719e
Repcak Mer <58186545+Kacperem@users.noreply.github.com>
2023-07-29 11:10:06 +0200

    add router

--- FRONTEND/memory_palace/src/App.tsx
+++ FRONTEND/memory_palace/src/App.tsx
@@ -1,18 +1,23 @@
-import './App.css'
-import { BrowserRouter} from "react-router-dom";
-import Navbar from './components/Navbar';
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 
 function App() {
-
   return (
     <>
       <BrowserRouter>
-      <Navbar />
-
-      
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/signin" element={<Login />} />
+          <Route path="/signon" element={<Register />} />
+        </Routes>
       </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ FRONTEND/memory_palace/src/pages/Login.tsx
@@ -0,0 +1,5 @@
+export default function Login() {
+  return (
+    <div>TODO Login form</div>
+  )
+}
