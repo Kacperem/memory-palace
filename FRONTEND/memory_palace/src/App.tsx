ec737e2602ceb1a92a2d9a2b72ecccda55d8da46
Repcak Mer <58186545+Kacperem@users.noreply.github.com>
2023-07-31 10:52:52 +0200

    add footer

--- FRONTEND/memory_palace/src/App.tsx
+++ FRONTEND/memory_palace/src/App.tsx
@@ -4,6 +4,7 @@ import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
+import Footer from "./components/Footer";
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
         </Routes>
+        <Footer />
       </BrowserRouter>
     </>
   );
--- FRONTEND/memory_palace/src/pages/Register.tsx
+++ FRONTEND/memory_palace/src/pages/Register.tsx
@@ -1,7 +1,6 @@
 import * as React from 'react';
 import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
-import CssBaseline from '@mui/material/CssBaseline';
 import TextField from '@mui/material/TextField';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import Checkbox from '@mui/material/Checkbox';
@@ -28,7 +27,6 @@ export default function Register() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <Grid container component="main" sx={{ height: '100vh' }}>
-        <CssBaseline />
         <Grid
           item
           xs={false}
