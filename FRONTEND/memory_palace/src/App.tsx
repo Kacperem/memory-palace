7576c082de78584459ecac72d507bd647d2de0c7
Repcak Mer <58186545+Kacperem@users.noreply.github.com>
2023-08-07 23:21:07 +0200

    add hero, features in landing page

--- FRONTEND/memory_palace/src/App.tsx
+++ FRONTEND/memory_palace/src/App.tsx
@@ -5,16 +5,18 @@ import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Footer from "./components/Footer";
+import CssBaseline from "@mui/material/CssBaseline";
 
 function App() {
   return (
     <>
       <BrowserRouter>
+      <CssBaseline />
         <Navbar />
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
+          <Route path="" element={<Home />} />
+          <Route path="login" element={<Login />} />
+          <Route path="register" element={<Register />} />
         </Routes>
         <Footer />
       </BrowserRouter>
