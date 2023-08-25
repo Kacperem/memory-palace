6fe3388c4b5240ae8d4f636d75fecb369fa6456f
mike-papa <73196026+mike-papa@users.noreply.github.com>
2023-08-25 22:42:18 +0200

    Add authorization

--- BACKEND/MemoryPalaceAPI/Entities/TwoDigitSystem.cs
+++ BACKEND/MemoryPalaceAPI/Entities/TwoDigitSystem.cs
@@ -4,5 +4,8 @@
     {
         public int Id { get; set; }
         public virtual List<TwoDigitElement> TwoDigitElements { get; set; }
+
+        public int? CreatedById { get; set; }
+        public virtual User CreatedBy { get; set; }
     }
 }
--- BACKEND/MemoryPalaceAPI/Models/TwoDigitSystemDto.cs
+++ BACKEND/MemoryPalaceAPI/Models/TwoDigitSystemDto.cs
@@ -5,6 +5,7 @@ namespace MemoryPalaceAPI.Models
     public class TwoDigitSystemDto
     {
         public int Id { get; set; }
+        public int CreatedById { get; set; }
         public List<TwoDigitElementDto> TwoDigitElements { get; set; }
     }
 }
