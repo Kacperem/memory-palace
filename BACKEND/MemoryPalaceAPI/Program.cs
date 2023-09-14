9f46108f23478ee42b8d5580e47029bd31957bcf
Repcak Mer <58186545+Kacperem@users.noreply.github.com>
2023-09-14 13:01:48 +0200

    Add auth, Update Services

--- BACKEND/MemoryPalaceAPI/Program.cs
+++ BACKEND/MemoryPalaceAPI/Program.cs
@@ -175,11 +175,11 @@ app.UseMiddleware<SwaggerBasicAuthMiddleware>();
 app.UseSwagger();
 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "SecureSwagger v1"));
 
-app.UseMiddleware<ApiKeyMiddleware>();
+
 app.UseHttpsRedirection();
 
 app.UseCors(MyAllowSpecificOrigins);
-
+app.UseMiddleware<ApiKeyMiddleware>();
 app.UseAuthentication();
 app.UseAuthorization();
 
